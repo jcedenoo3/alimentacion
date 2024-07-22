28320450dc3b79b14584008d5b2c9a804bf10fda
jcedenoo3 <jimecede2004@gmail.com>
2024-07-21 22:15:48 -0400

    Add files via upload

---
+++ Alimentacion.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestionalimentacionn;
+
+/**
+ *
+ * @author jimec
+ */
+class Alimentacion {
+
+    int length;
+    public String animal;
+    public String horario;
+    public String alimento;
+    public int frecuencia;
+
+    Alimentacion(String nombre, String horario, String alimento, int frecuencia) {
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getAlimento() {
+        return alimento;
+    }
+
+    public void setAlimento(String alimento) {
+        this.alimento = alimento;
+    }
+
+    public int getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(int frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    class get {
+
+        public get() {
+        }
+    }
+    
+}
